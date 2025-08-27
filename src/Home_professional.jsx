574cdd685e0bb529b6082b987fcb6e4b782f3e88
ArmaanBudhiraja <armaan.collegework@gmail.com>
2025-08-28 01:28:35 +0530

    Updated

--- src/Home_professional.jsx
+++ src/Home_professional.jsx
@@ -1,5 +1,6 @@
 import './Home_professional.css'
 import FileIcon from './images/File.png'
+import ProfPhoto from './images/photo.jpg'
 
 function Home_professional() {
     return (
@@ -14,15 +15,32 @@ function Home_professional() {
                     <a href="#" className='light'>Professional</a>
                     <a href="#" className='dark'>Chaotic</a>
                 </div>
-                <div className='Content'>
-                    <h2>Hi, I am</h2>
-                    <h1>ARMAAN</h1>
-                    <h1>BUDHIRAJA</h1>
-                    <h2 className='down_h2'>Creative Technologist</h2>
-                    <button className='resume_button'>
-                        <img src={FileIcon} alt='resume_icon' className='resume_icon'/>
-                        <p className='Resume_text'>Resume</p>
-                    </button>
+                <div className='Main_Content'>
+                    <div className='Content'>
+                        <h2>Hi, I am</h2>
+                        <h1>ARMAAN</h1>
+                        <h1>BUDHIRAJA</h1>
+                        <h2 className='down_h2'>Creative Technologist</h2>
+                        <button className='resume_button'>
+                            <img src={FileIcon} alt='resume_icon' className='resume_icon'/>
+                            <p className='Resume_text'>Resume</p>
+                        </button>
+                        <div className='Button_line'>
+                            <button className='Hire_me'>Hire Me</button>
+                            <button className='github_button'>
+                                <img src='https://img.icons8.com/ios-glyphs/50/000000/github.png' alt='github_icon' className='github_icon'/>
+                            </button>
+                            <button className='linkedin_button'>
+                                <img src='https://img.icons8.com/ios-filled/50/000000/linkedin.png' alt='linkedin_icon' className='linkedin_icon'/>
+                            </button>
+                        </div>
+                        <div className='hire_msg'>
+                            <p>plss 😭</p>
+                        </div>
+                    </div>
+                    <div className='Photo'>
+                        <img src={ProfPhoto} alt='Profile_Photo' className='Profile_photo'/>
+                    </div>
                 </div>
             </div>
         </>
